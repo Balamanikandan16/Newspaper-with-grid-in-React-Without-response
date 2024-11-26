e221720837ed64d429b637a3c7bbb2004e00f0fb
Balamanikandan16 <balamanikandan.s5555@gmail.com>
2024-11-26 18:59:23 +0530

    Add files via upload

---
+++ onepage.tsx
@@ -0,0 +1,122 @@
+import React,{Component} from "react";
+import './onepage.css'
+class OnePage extends Component{
+render(){
+    return(
+        <header>
+     <section className="head"> 
+     <div className="subhead">
+        <p>Quis nostrud exercitatio nullamco laboris</p>
+     </div>
+        <div className="subhead1">
+            <h1>NewsPaper</h1>
+        </div>
+        <div className="subhead2">
+            <h4>Lorem ipsum</h4>
+            <p>Lorem ipsum dolor sit amet,consectetur adipisicing elit. Nemo cum voluptates vel sint! Lorem ipsum dolor sit amet</p>
+        </div>
+    </section>
+    <footer>
+        <div className="line">
+            <hr></hr>
+            <h3 className="lineh3">Saturday,Nov23 2024</h3>
+            <hr></hr>
+        </div>
+        <div className="newshead">
+            <h1 className="newshead1">Nuture Newspaper</h1>
+            <h1 className="newshead2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, totam necessitatibus vitae veniam autem facilis! Lorem ipsum dolor sit amet consectetur Lorem, ipsum dolor.</h1>
+            </div>
+
+        <div className="grid">
+            <div className="box1">
+                <div className="grid1bg"></div>
+                <p className="grid1p">Lorem ipsum dolor sit amet consectetur adipisicing elit. At libero sequi cupiditate magnam excepturi expedita? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis modi sunt perferendis odit quod consequatur vero molestiae Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et ut debitis, blanditiis corporis, magnam at natus error sunt sed reprehenderit modi deserunt accusamus nam. Ipsam reprehenderit nostrum eligendi labore perspiciatis. .<u> psum quos cumque ut beatae quis sunt sint sed recusandae Lorem, ipsum dolor.</u></p>
+            </div>
+
+            <div className="box2">
+                <div>
+                    <hr></hr>
+                <h2>Excepture sint occaecat ku Lorem,ok
+                ipsum.cupiditate non</h2>
+                <hr></hr>
+                </div>
+                <div className="box2grid">
+                    <div className="box2grid1">Lorem ipsum dolor Loremit amet consecteturudantium itaque cumque,repellat vitae hic nam.Eveniet, unde dicta accusantium delectus maiores eum aperiam quibusdam dolorem! Sed, amet. sit amet, consectetur adipisicing elit. Minus natusquae volu tempore mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo consectetur corrupti temporibus earum, cum fugit quo vitae et recusandae culpa consequuntur a illum ipsam! Accusamus earum magnam ex? Tempora, quisquam! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio magni vero inventore, iste quisquam quam quo praesentium voluptates eius cumque sapiente officiis quae tempore , aut repudiandae quasi iusto, iure cupiditate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus magni a autem cupiditate totam voluptatum, ad amet odit. Libero saepe vero quidem voluptatum nihil iste sint officia esse doloribus quo. </div>
+                    <div className="box2grid2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil eum alias ad, doloribus voluptatibus consequuntur vero atque in rerum sunt et sequi pariatur modi illum laborum aut est dolor? Lorem ipsum dolor sit amet consectetur adipisicing elit.tia laboriosam labore blanditiis odit. Eum quae voluptatum qui? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, eum? Possimus veritatis saepe mollitia est iusto amet. </div>
+                    <div className="box2grid4">
+                        <div className="box2g1"></div>
+                        <div className="box2g2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum provident in ut pariatur omnis eaque consequatur asperiores magni possimus dicta beatae, dolores harum aliquid sunt porro a consequuntur voluptatem inventore!</div>
+                    </div>
+            <div className="box2grid5">
+                <hr></hr>
+                <h1>Lorem ipsum dolor sit amet consectetur adip Lorem dolor</h1>
+                <hr></hr>
+                </div>
+                <div className="box2grid6">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores, ullam mollitia voluptatem reiciendis quidem animi soluta eaque blanditiis itaque inventore porro rem! Vel possimus saepe, aliquid eveniet rerum asperiores iusto?</p>
+                </div>
+                <div className="box2grid7">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing in voluptas exercitationem impedit expedita, eos molestiae dolorum minus qui corporis dolores quibusdam, iste assumenda quidem id magni possimus!</p>
+                </div>
+            </div>
+            </div>
+
+            <div className="box3">
+                <h1>Lorem ipsum dolor sitLorem dolor</h1>
+                <hr></hr>
+                <h2>Lorem ipsum dolor sit amet, consectetur adipisicing.</h2>
+                <hr></hr>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum tempore reiciendis quaerat sunt ab animi ipsa laboriosam odio eum illum expedita quibusdam, deserunt maiores culpa possimus a quidem perferendis atque. Lorem ipsum dolor, sit amet consectetur adipisicing elit. A blanditiis molestias sequi, neque repellendus, optio delectus reiciendis pariatur nisi distinctio, placeat asperiores exercitationem. Totam ut est dolorum alias architecto. Corrupti commodi expedita eaque voluptatem explicabo repellendus architecto distinctio accusamus! Nihil natus possimus quod omnis unde cumque dignissimos minus! Provident totam sunt quia mollitia vitae ab culpa architecto ullam dolores iusto ducimus, obcaecati quibusdam facere sed, omnis eveniet ut quam animi iure. Labore, consectetur. Officiis accusamus ea modi libero error sequi eaque quis quas provident vel omnis ex mollitia neque fugiat, tenetur, quam, quibusdam velit eum tempore maxime. Delectus, perspiciatis animi Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et, sapiente Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, debitis hic quasi natus minus incidunt deserunt id reiciendis neque sed nihil numquam, modi recusandae reprehenderit ipsam aspernatur vero obcaecati aperiam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquid, quisquam, soluta natus in deleniti ipsum, consequatur enim ratione numquam perspiciatis voluptatibus nobis assumenda delectus atque repellat minima. Hic, dolorum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, ex. Vitae optio ipsum iusto dolor omnis libero! beatae quas doloremque corporis, sunt quo consectetur porro praesentium est natus odio debitis inventore saepe quod repellendus accusantium, maxime quia? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita quasi eaque deleniti ullam quis cupiditate, repudiandae perferendis beatae obcaecati a aspernatur perspiciatis commodi accusantium nam ratione voluptas porro non omnis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus reiciendis qui pariatur voluptatibus exercitationem cum nemo vero consectetur? Eaque consequuntur unde labore at nam necessitatibus temporibus eveniet esse vel distinctio.</p>
+                </div>
+            <div className="box4">
+                <div className="box4grid">
+                <div className="box4grid1">
+                    <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit</h1>
+                </div>
+                <div className="box4grid2">Lorem ipsum dolor, bore et minus fugiat ex quae ipsa reiciendis velit tempore nostrum. Reiciendis minus quasi molestias optio! Accusantium dignissimos enim ea, tenetur cumque eligendi corporis atque? Quasi, quas? Maxime laudantium magni consectetur quaerat</div>
+                <div className="box4grid3">Lorem, ipsum dolor ncidunt ipsa vel ea expedita tenetur facere! Sit fuga soluta amet neque mollitia cumque nihil consequuntur maiores odio et exercitationem qui dolores nisi adipisci suscipit esse incidunt sunt consequatur harum ipsam asperiores archit</div>
+                <div className="box4grid4">Lorem ipsum dolor uptatuConsequuntur dolorum eum vel provident animi nam ipsa quidem. Eius nemo enim culpa, eum voluptatibus labore ut, officia architecto, aliquid ratione iure id perferendis voluptas earum exercitationem tempora suscipit nihil debitis!</div>
+            </div>
+            </div>
+            <div className="box5">
+                <div className="box5grid">
+                    <div className="box5grid7">
+                        <hr></hr>
+                    <h1>Task panamudila, ethu yeppo mudithu therila </h1>
+                    <hr></hr>
+                    </div>
+                <div className="box5grid1">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto dolorem facere quae nece</p>
+                </div>
+                <div className="box5grid2">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto dolorem facere quae nece</p>
+                </div>
+                <div className="box5grid3">
+                </div>
+                <div className="box5grid4">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto dolorem facere quae nece</p>
+                </div>
+                <div className="box5grid5">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto dolorem facere quae nece</p>
+                </div>
+                <div className="box5grid6"> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto dolorem facere quae nece</p></div>
+            </div>
+            </div>
+            <div className="box6">
+                <div className="box6grid">
+                <div className="box6grid1">
+                    <hr></hr>
+                    <h1>Last box daa samy.</h1>
+                    <hr></hr>
+                </div>
+                <div className="box6grid2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quas cum veritatis autem tenetur earum, saepe ea explicabo itaque sequi repudiandae harum aspernatur, ut delectus? Quos repellat perferendis magni quasi Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae velit, magnam quia quasi qui blanditiis! Voluptatem est cumque qui, error, quasi consequatur quibusdam accusamus minima ipsaium!</div>
+                <div className="box6grid3"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem animi ducimus ut, ratione quo nostrum, libero, deleniti fugit deserunt perferendis eligendi inventore tenetur provident optio eius nobis dignissimos asperiores ipsam Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur, temporibus necessitatibus. Consectetur tempore veniam, ea repudiandae labore voluptate delectu!</div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    </header>
+    )
+}
+}
+export default OnePage;
